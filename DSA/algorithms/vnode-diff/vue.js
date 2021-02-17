00cd204c8d3ceec9366e501265d3c18eb41473a8
Zen <kefengong@gmail.com>
2021-02-17 22:43:00 +0800

    diff

---
+++ DSA/algorithms/vnode-diff/vue.js
@@ -0,0 +1,18 @@
+
+/**
+ * @desc snabbdom & vue2 中采用的双端比较算法
+ * @param {[number]} oldChildren 
+ * @param {[number]} newChildren 
+ */
+export function diff(oldChildren, newChildren) {
+  let oldStartIdx = 0;
+  let newStartIdx = 0;
+  let oldEndIdx = 0;
+  let newEndIdx = 0;
+
+  while(oldStartIdx <= oldEndIdx && newStartIdx <= newEndIdx) {
+    
+  }
+
+  return oldChildren
+}
