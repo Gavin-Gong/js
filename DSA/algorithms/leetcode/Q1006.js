de6423a8b9fa7473439e084ae59c5241a0f23cb3
Gavin Gong <kefengong@gmail.com>
2021-04-01 19:00:37 +0800

    update Q1006

--- DSA/algorithms/leetcode/Q1006.js
+++ DSA/algorithms/leetcode/Q1006.js
@@ -11,15 +11,11 @@ export function clumsy(N) {
     const c = N - i - 2;
     const d = N - i - 3;
     if (res === null) {
-      res = calcList([a, b <= 0 ? 1 : b, c <= 0 ? 1 : c, d <= 0 ? 0 : -d]);
+      res = (((a * (b <= 0 ? 1 : b)) / (c <= 0 ? 1 : c)) >> 0) + (d <= 0 ? 0 : d);
     } else {
-      res = res - calcList([a, b <= 0 ? 1 : b, c <= 0 ? 1 : c, d <= 0 ? 0 : d]);
+      res = res - ((((a * (b <= 0 ? 1 : b)) / (c <= 0 ? 1 : c)) >> 0) - (d <= 0 ? 0 : d));
     }
     i = i + 4;
   }
   return res;
-}
-
-function calcList(list) {
-  return (((list[0] * list[1]) / list[2]) >> 0) - list[3];
 }
